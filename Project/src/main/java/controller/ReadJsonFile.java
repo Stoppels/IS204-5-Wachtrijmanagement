f37b4cd8b77fa0236a39a138f73907b75aee4c40
Nick Shayan <Nick.Shayan@hva.nl>
2015-10-12 23:49:59 +0200

    Implement platform independent file seperator for File lookup.

--- Project/src/main/java/controller/ReadJsonFile.java
+++ Project/src/main/java/controller/ReadJsonFile.java
@@ -8,6 +8,7 @@ package controller;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.File;
 import java.util.ArrayList;
 import model.JsonLine;
 
@@ -17,66 +18,66 @@ import model.JsonLine;
  */
 public class ReadJsonFile {
 
-    public static void main(String[] args) {
-        readJsonFile();
-    }
+	public static void main(String[] args) {
+		readJsonFile();
+	}
 
-    public static ArrayList<JsonLine> readJsonFile() {
+	public static ArrayList<JsonLine> readJsonFile() {
 
-        String filename = "recording001_short.json";
-        BufferedReader br = null;
-        JsonLine jsonLine;
-        ArrayList<JsonLine> jsonLines = new ArrayList<JsonLine>();
+		String filename = "recording001_short.json";
+		BufferedReader br = null;
+		JsonLine jsonLine;
+		ArrayList<JsonLine> jsonLines = new ArrayList<JsonLine>();
 
-        try {
-            int counter = 0;
-            String string;
-            String[] stringArray;
-            br = new BufferedReader(new FileReader("./Json\\" + filename));
+		try {
+			int counter = 0;
+			String string;
+			String[] stringArray;
+			br = new BufferedReader(new FileReader("Json" + File.separator + filename));
 
-            // extracting values from one line of JSON, puts it in JsonLine obj
-            while ((string = br.readLine()) != null && counter < 10000) {
-                stringArray = string.split(",");
-                for (int i = 0; i < stringArray.length; i++) {
-                    stringArray[i] = stringArray[i].replaceAll("[\\[\\]\"}]", "");
-                    stringArray[i] = stringArray[i].substring(stringArray[i].indexOf(':') + 1).trim();
-                    System.out.println("####################################################################################################println");
-                    System.out.println(stringArray[i]);
-                }
-                jsonLine = new JsonLine();
-                jsonLine.setTrack_id(Integer.parseInt(stringArray[0]));
-                jsonLine.setTimestamp(stringArray[1]);
-                jsonLine.setEvent_type(Integer.parseInt(stringArray[2]));
-                jsonLine.setAlive_status(Integer.parseInt(stringArray[3]));
-                jsonLine.setPosition(Double.parseDouble(stringArray[4]),
-                        Double.parseDouble(stringArray[5]));
-                jsonLine.setBbox(Float.parseFloat(stringArray[6]),
-                        Float.parseFloat(stringArray[7]),
-                        Float.parseFloat(stringArray[8]),
-                        Float.parseFloat(stringArray[9]),
-                        Float.parseFloat(stringArray[10]),
-                        Float.parseFloat(stringArray[11]));
-                System.out.println("####################################################################################################println");
-                System.out.println(jsonLine.toString());
-                jsonLines.add(jsonLine);
+			// extracting values from one line of JSON, puts it in JsonLine obj
+			while ((string = br.readLine()) != null && counter < 10000) {
+				stringArray = string.split(",");
+				for (int i = 0; i < stringArray.length; i++) {
+					stringArray[i] = stringArray[i].replaceAll("[\\[\\]\"}]", "");
+					stringArray[i] = stringArray[i].substring(stringArray[i].indexOf(':') + 1).trim();
+					System.out.println("####################################################################################################println");
+					System.out.println(stringArray[i]);
+				}
+				jsonLine = new JsonLine();
+				jsonLine.setTrack_id(Integer.parseInt(stringArray[0]));
+				jsonLine.setTimestamp(stringArray[1]);
+				jsonLine.setEvent_type(Integer.parseInt(stringArray[2]));
+				jsonLine.setAlive_status(Integer.parseInt(stringArray[3]));
+				jsonLine.setPosition(Double.parseDouble(stringArray[4]),
+						Double.parseDouble(stringArray[5]));
+				jsonLine.setBbox(Float.parseFloat(stringArray[6]),
+						Float.parseFloat(stringArray[7]),
+						Float.parseFloat(stringArray[8]),
+						Float.parseFloat(stringArray[9]),
+						Float.parseFloat(stringArray[10]),
+						Float.parseFloat(stringArray[11]));
+				System.out.println("####################################################################################################println");
+				System.out.println(jsonLine.toString());
+				jsonLines.add(jsonLine);
 
-            }
-        } catch (IOException e) {
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-            } catch (IOException ex) {
-            }
-        }
+			}
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+			} catch (IOException ex) {
+			}
+		}
 
-        System.out.println("####################################################################################################println");
-        if (!jsonLines.isEmpty()) {
-            for (JsonLine a : jsonLines) {
-                System.out.println(a.toString());
-            }
-        }
-        return jsonLines;
-    }
+		System.out.println("####################################################################################################println");
+		if (!jsonLines.isEmpty()) {
+			for (JsonLine a : jsonLines) {
+				System.out.println(a.toString());
+			}
+		}
+		return jsonLines;
+	}
 }
