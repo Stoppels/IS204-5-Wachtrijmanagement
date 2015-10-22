ff9fda1f5bd4a8f10d1665d78c4cb3b55b18d53b
Chris Verra <chrisverra@gmail.com>
2015-10-22 14:26:03 +0200

    canvas dots start at center of canvas

--- Project/src/main/webapp/resources/js/canvas.js
+++ Project/src/main/webapp/resources/js/canvas.js
@@ -1,3 +1,4 @@
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -42,17 +43,20 @@ xAxis.stroke();
  * @param {int} y
  * @returns {void}
  */
-function drawDot(dotname, xPos, yPos, color){
+function drawDot(dotname, xPos, yPos, color, stringName){
     // create object
     var dotname = canvas.getContext("2d");
     dotname.beginPath();
-    dotname.arc(xPos, yPos, dotSize, 0, 2 * Math.PI);
+    dotname.arc((xPos + centerX), (-yPos + centerY), dotSize, 0, 2 * Math.PI);
     dotname.stroke();
     dotname.fillStyle = '#ff00ff';
     dotname.fillStyle = "#"+color;
     dotname.fill();
+
 }
 
-drawDot("test", 500, 20, "ff0000");
-
-
+drawDot("test", 100, -20, "ff0000");
+drawDot("foo", -100, 10, "00ff00");
+drawDot("bar", 20, -30, "0000ff");
+drawDot("blaat", 80, 40, "f0f0f0");
+drawDot("asdf", 40, 200, "ff00ff")
