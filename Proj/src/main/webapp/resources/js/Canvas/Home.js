16e129ee5e4075a70e9b33772979b7736cf36c06
Menzies <Stefan_schenk@hotmail.com>
2015-11-17 22:12:24 +0100

    Background transparency improved
    Former-commit-id: 1f92412c3ad6d499c7e0c92148398870c5c3b6b5

--- Proj/src/main/webapp/resources/js/Canvas/Home.js
+++ Proj/src/main/webapp/resources/js/Canvas/Home.js
@@ -6,7 +6,7 @@ Author     : Stefan */
 window.onload = function showLogo() {
     var xOffset  = 700;
     var yOffset  = -300;
-    drawRectangle(0 + xOffset, 200 + yOffset, 190, 50, 45, '#FF0000');
+    drawRectangle(0 + xOffset, 200 + yOffset, 190, 50, 45, '#FF0000' );
     drawRectangle(0 + xOffset, 200 + yOffset, 190, 50, 135, '#FF0000');
     drawRectangle(0 + xOffset, 0 + yOffset, 190, 50, 45, '#FF0000');
     drawRectangle(0 + xOffset, 0 + yOffset, 190, 50, 135, '#FF0000');
