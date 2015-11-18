539a7c116a12d044871cf0217dafe99e1b188215
Menzies <Stefan_schenk@hotmail.com>
2015-11-18 11:41:42 +0100

    Cheered up the 404 page a bit
    Former-commit-id: 6bda5ba2386bd09aabfe223d6448f025d0c6e2c7

--- Proj/src/main/webapp/resources/js/Canvas/Error.js
+++ Proj/src/main/webapp/resources/js/Canvas/Error.js
@@ -6,14 +6,14 @@ Author     : Stefan */
 window.onload = function showLogo() {
     var xOffset  = 700;
     var yOffset  = -300;
-    drawRectangle(0 + xOffset, 200 + yOffset, 190, 50, 45, '#FF0000' );
-    drawRectangle(0 + xOffset, 200 + yOffset, 190, 50, 135, '#FF0000');
-    drawRectangle(0 + xOffset, 0 + yOffset, 190, 50, 45, '#FF0000');
-    drawRectangle(0 + xOffset, 0 + yOffset, 190, 50, 135, '#FF0000');
-    drawRectangle(0 + xOffset, -200 + yOffset, 190, 50, 45, '#FF0000');
-    drawRectangle(0 + xOffset, -200 + yOffset, 190, 50, 135, '#FF0000');
-    drawText("404", -650 + xOffset, 225 + yOffset, 80);
-    drawText("Error", -700 + xOffset, 125 + yOffset, 80);
+    drawRectangle(100 + xOffset, 0 + yOffset, 190, 50, 45, '#FF0000' );
+    drawRectangle(100 + xOffset, 0 + yOffset, 190, 50, 135, '#FF0000');
+    drawRectangle(-100 + xOffset, 0 + yOffset, 190, 50, 45, '#FF0000');
+    drawRectangle(-100 + xOffset, 0 + yOffset, 190, 50, 135, '#FF0000');
+    drawRectangle(-300 + xOffset, 0 + yOffset, 190, 50, 45, '#FF0000');
+    drawRectangle(-300 + xOffset, 0 + yOffset, 190, 50, 135, '#FF0000');
+    drawText("404 Error", -650 + xOffset, 225 + yOffset, 80);
+    drawText("This is not the page you are looking for", -700 + xOffset, 125 + yOffset, 40);
 };
 
 function play() {
